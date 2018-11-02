84dc1c55940b1d705d9a771f61fdb9bba47a1579
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-11-02 13:30:49 +0300

    setup minimal landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
-import '../assets/stylesheets/App.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './common/Header';
+import HomePage from "./Homepage";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+            <div className='gr1d99'>
+                <Header/>
+                <div className='content'>
+                    <Route path='/' exact component={HomePage}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
   }
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <div className='col-md-6 offset-md-3'>
+                <h3 className='text-center pt-5'>Efficiently manage your dairy cattle through data</h3>
+            </div>
+        </div>
+    )
+}
